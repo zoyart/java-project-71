58ccfd6037a94bb4012e8ef66d5b312175cf5507
Artur Tsoi <xzoyart@gmail.com>
2024-06-19 16:15:25 +0500

    make App class static

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -14,7 +14,7 @@ import java.util.concurrent.Callable;
         version = "gendiff 1.0",
         description = "Compares two configuration files and shows a difference.")
 @Getter
-public class App implements Callable<Integer> {
+public final class App implements Callable<Integer> {
     @Parameters(paramLabel = "filePath1", description = "path to first file")
     private String filePath1;
 
